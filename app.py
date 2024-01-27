6e414946e2281e1cf233b133af3b0bcd373ed37f
Hamza Zaidi <cookiestein3@gmail.com>
2024-01-27 13:17:44 -0500

    update file upload again

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from views import views
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'file' in request.files:
-#         file = request.files['file']
-#         return "File uploaded successfully!"
-#     return "No file provided."
+@app.route('/upload', methods=['POST'])
+def upload():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
    app.run(debug=True, port=8000)
