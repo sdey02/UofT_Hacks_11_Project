acb24415289a5f40253e638cbcb6fd7b7ea7b8a4
Hamza Zaidi <cookiestein3@gmail.com>
2024-01-27 11:50:54 -0500

    Fix basic file upload

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, Blueprint, render_template, request, redirect, url_for
-from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/views")
 
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'file' in request.files:
-#         file = request.files['file']
-#         return "File uploaded successfully!"
-#     return "No file provided."
+@app.route('/upload', methods=['POST'])
+def upload():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
    app.run(debug=True, port=8000)
-
