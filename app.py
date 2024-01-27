c98275b873e2a3b179fcb45cebb088e35db04e36
Hamza Zaidi <cookiestein3@gmail.com>
2024-01-27 04:13:55 -0500

    Basic file upload

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' in request.files:
+        file = request.files['file']
+        return "File uploaded successfully!"
+    return "No file provided."
 
 if __name__ == '__main__':
    app.run(debug=True, port=8000)
