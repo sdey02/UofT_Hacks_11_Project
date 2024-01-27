9fe1c5d4a9f63e13c14f573a71768219549e6bf5
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-01-27 11:30:14 -0500

    routing

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, Blueprint, render_template, request, redirect, url_for
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/views")
 
-@app.route("/")
-def home():
-   return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' in request.files:
-        file = request.files['file']
-        return "File uploaded successfully!"
-    return "No file provided."
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     if 'file' in request.files:
+#         file = request.files['file']
+#         return "File uploaded successfully!"
+#     return "No file provided."
 
 if __name__ == '__main__':
    app.run(debug=True, port=8000)
---
+++ views.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+   return render_template('index.html')
+
+@views.route("/register")
+def register():
+    return render_template('register.html')
