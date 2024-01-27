7d16317da48140879301b8aa407a6330634d766a
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-01-27 04:08:21 -0500

    refactor

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
-from views import views
+from flask import Flask, Blueprint, render_template, request
 
 app = Flask(__name__)
+
+views = Blueprint(__name__, "views")
+
+
+@views.route("/")
+def home():
+    return render_template('index.html')
+
 app.register_blueprint(views, url_prefix="/")
 
 
 if __name__ == '__main__':
    app.run(debug=True, port=8000)
+
