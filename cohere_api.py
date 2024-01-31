79bf573f2ae9551bde6035a5311c0e807dca7a8c
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-31 15:39:41 -0500

    Fix location with cohere

--- cohere_api.py
+++ cohere_api.py
@@ -8,58 +8,35 @@ import certifi
 import geopy.geocoders
 import sys
 import json
-
-
-co = cohere.Client(load(open("keys/config.json", "r"))["token"]) #! Cohere API Key 
-
-#! Cohere model extracts location from user input.
-# location = co.generate(   
-#     model='command',
-#     prompt=f'I am going to give you some text. I want you to output one line of information from it in the form "City, Country". {user_input}',
-#     max_tokens=300,
-#     temperature=0.9,
-#     k=0,
-#     stop_sequences=[],
-#     return_likelihoods='NONE')
-
-# location_data = format(location.generations[0].text)
-
-#! Cohere model generates a caption for photo.
-# caption = co.generate(
-#     model='command',
-#     prompt=f'I am going to give you some text describing the events that took place in a photo. Using that text, output only a short, catchy caption for the photo inside double quotes.{user_input}',
-#     max_tokens=300,
-#     temperature=0.9,
-#     k=0,
-#     stop_sequences=[],
-#     return_likelihoods='NONE')
-
-# caption_data = format(caption.generations[0].text)
-
-#! Geopy longitude and lattitude conversion.
-ctx = ssl.create_default_context(cafile=certifi.where())
-geopy.geocoders.options.default_ssl_context = ctx
-
-# geolocator = Nominatim(user_agent="my_request")
-# location_geopy = geolocator.geocode(location_data)
-# latitude = location_geopy.latitude
-# longitude = location_geopy.longitude
-
-# print(latitude)
-# print(longitude)
+import spacy
 
 def test(caption):
-    location = co.generate(   
-        model='command',
-        prompt=f'I am going to give you some text. I want you to output one line of information from it in the form "City, Country". {caption}',
-        max_tokens=300,
-        temperature=0.9,
-        k=0,
-        stop_sequences=[],
-        return_likelihoods='NONE')
+    #* Cohere model extracts location from user input (Deprecated Switching to SpaCy to get location data)
+    # location = co.generate(   
+    #     model='command',
+    #     prompt=f'I am going to give you some text. I want you to output one line of information from it in the form "City, Country". {user_input}',
+    #     max_tokens=300,
+    #     temperature=0.9,
+    #     k=0,
+    #     stop_sequences=[],
+    #     return_likelihoods='NONE')
+
+    # location_data = format(location.generations[0].text)
+
+    #! spaCY extrats location data from sentance.
+    nlp = spacy.load('en_core_web_sm')
+    array = []
+
+    doc = nlp(caption)
+    for entity in doc.ents:
+        if entity.label_ == 'GPE':
+            array.append(entity.text)
+
+    location = ", ".join(array)
     
-    location_data = format(location.generations[0].text)
-    
+    #! Cohere model generates a caption for photo.
+    co = cohere.Client(load(open("keys/config.json", "r"))["token"]) #! Cohere API Key 
+
     caption = co.generate(
         model='command',
         prompt=f'I am going to give you some text describing the events that took place in a photo. Using that text, output only a short, catchy caption for the photo inside double quotes.{caption}',
@@ -70,12 +47,15 @@ def test(caption):
         return_likelihoods='NONE')
     
     caption_data = format(caption.generations[0].text)
+
+    #! Geopy longitude and lattitude conversion.
+    ctx = ssl.create_default_context(cafile=certifi.where())
+    geopy.geocoders.options.default_ssl_context = ctx
     
     geolocator = Nominatim(user_agent="my_request")
-    location_geopy = geolocator.geocode(location_data)
+    location_geopy = geolocator.geocode(location)
     latitude = location_geopy.latitude
     longitude = location_geopy.longitude
+    
 
     return [caption_data, latitude, longitude]
-
-# print(test("We visited the Grand Canyon today which is located in Arizona, USA and saw a mountain lion"))
